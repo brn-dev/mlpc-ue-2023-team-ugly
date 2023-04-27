d27c5e30945ab626d2a547cb1aeb88ba9959c610
Dominik Baron <dominik.b4ron@gmail.com>
2023-04-27 14:07:55 +0200

    Initial commit

---
+++ lib/ds/bird_dataset.py
@@ -0,0 +1,13 @@
+from torch.utils.data import Dataset
+from torch.utils.data.dataset import T_co
+
+class BirdDataset(Dataset):
+
+    def __init__(self):
+        pass
+
+    def __getitem__(self, index) -> T_co:
+        pass
+
+
+
