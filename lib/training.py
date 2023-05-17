bf25735c46c9c28192bbd8e23343ee3fdc5c7925
Leon Alexander Akkad <leon@akkad.at>
2023-05-17 18:00:00 +0200

    Changed training function to use balanced dataset

--- lib/training.py
+++ lib/training.py
@@ -4,6 +4,7 @@ from typing import Any
 from tqdm import tqdm
 import dill as pkl
 import numpy as np
+from sklearn.model_selection import StratifiedKFold
 from lib.data_preprocessing import normalize_data
 
 # Parameters: train_data, train_labels; Returns: Model
@@ -21,8 +22,8 @@ def get_baseline(labels: np.ndarray) -> float:
 
 
 def train_with_cv(
-        data_folds: np.ndarray,
-        labels_folds: np.ndarray,
+        data_train: np.ndarray,
+        labels_train: np.ndarray,
         create_and_train_func: CreateAndTrainFunc,
         eval_func: EvalFunc,
         n_folds=10):
@@ -36,18 +37,16 @@ def train_with_cv(
         train_accuracies = np.array([])
         train_b_accuracies = np.array([])
         accuracies = dict()
-        for fold in range(n_folds):
-            data_validation = data_folds[fold]
-            labels_validation = labels_folds[fold]
+        skf = StratifiedKFold(n_splits=n_folds)
+        for i, (train_idx, val_idx) in enumerate(skf.split(data_train, labels_train)):
+            data_validation = data_train[val_idx]
+            labels_validation = labels_train[val_idx]
+            data_train_split = data_train[train_idx]
+            labels_train_split = labels_train[train_idx]
 
-            data_train = data_folds[np.setdiff1d(range(n_folds), fold)] \
-                .reshape((-1, data_folds.shape[-2], data_folds.shape[-1]))
-            labels_train = labels_folds[np.setdiff1d(range(n_folds), fold)] \
-                .reshape((-1, labels_folds.shape[-1]))
+            data_train_normalized, data_validation_normalized = normalize_data(data_train_split, data_validation)
 
-            data_train_normalized, data_validation_normalized = normalize_data(data_train, data_validation)
-
-            model = create_and_train_func(data_train_normalized, labels_train, k=k)
+            model = create_and_train_func(data_train_normalized, labels_train_split, k=k)
             valid_acc, valid_b_acc = eval_func(model, data_validation_normalized, labels_validation)
             train_acc, train_b_acc = eval_func(model, data_train_normalized, labels_train)
             valid_accuracies = np.append(valid_accuracies, valid_acc)
